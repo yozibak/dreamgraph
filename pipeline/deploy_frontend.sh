6a3eadde604cbbe6ccc58ed1402e4704ae23ff0c
Katsumi Yoshida <yozibak@gmail.com>
2024-04-15 13:26:34 +0900

    separate deploy script

--- pipeline/deploy_frontend.sh
+++ pipeline/deploy_frontend.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# get the output values
 stack_name="serverless-chat-$1"
-
-api_gateway_endpoint=$(
-    aws cloudformation describe-stacks \
-        --stack-name "$stack_name" \
-        --query "Stacks[0].Outputs[?OutputKey=='GraphQLApiEndpoint'].OutputValue" \
-        --output text
-    )
-
-api_key=$(
-    aws cloudformation describe-stacks \
-        --stack-name "$stack_name" \
-        --query "Stacks[0].Outputs[?OutputKey=='GraphQLApiKey'].OutputValue" \
-        --output text
-    )
 
 cloudfront_distribution_id=$(
     aws cloudformation describe-stacks \
@@ -31,19 +16,8 @@ s3_bucket_name=$(
         --output text
     )
 
-echo "API Gateway URL: $api_gateway_endpoint"
-echo "CloudFront Distribution ID: $cloudfront_distribution_id"
-echo "S3 Bucket Name: $s3_bucket_name"
-
-# build & deploy
 cd frontend/app
-pnpm i
-rm .env && touch .env
-echo "VITE_API_ENDPOINT=$api_gateway_endpoint" >> .env
-echo "VITE_API_KEY=$api_key" >> .env
-pnpm build
-cd dist
-aws s3 sync . s3://$s3_bucket_name/
+aws s3 sync dist s3://$s3_bucket_name/
 echo "--upload complete--"
 
 # cloudfront invalidation
