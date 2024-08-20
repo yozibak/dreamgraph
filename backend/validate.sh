466ce90805bc4544985caeaf05559c29caaf3d2b
Katsumi Yoshida <yozibak@gmail.com>
2024-08-21 09:33:43 +1000

    validate before push

--- backend/validate.sh
+++ backend/validate.sh
@@ -1,4 +1,4 @@
 sam validate --template backend/api/template.yaml
 sam validate --template backend/db/template.yaml
 sam validate --template backend/auth/template.yaml
-sam validate --template backend/host/template.yaml
+sam validate --template backend/template.yaml
